50c3f6c22934facc8d9d398e86ae495226264fad
007GHOSTRIDER <paulr2@uw.edu>
2026-05-24 22:03:52 -0700

    auth sign up and login

--- daos/userDao.js
+++ daos/userDao.js
@@ -6,5 +6,17 @@ const SALT_ROUNDS = 10;
 // Creates a new user in the database
 export const createUser = async (userObj) => {
     const hashedPassword = await bcrypt.hash(userObj.password, SALT_ROUNDS);
-    return models.User.create({...userObj, password: hashedPassword});
+    return models.User.create({
+        ...userObj, 
+        password: hashedPassword,
+        roles: 'user'
+    });
+};
+
+export const getUserEmail = async (email) => { 
+    try {
+        return await User.findOne({email});
+    } catch (e) {
+        return null;
+    }
 };
---
+++ routes/auth.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/user';
+import isAuthorized from '../middleware/isAuthorized'
+import { createUser, getUserByEmail } from '../daos/userDao';
+
+const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+// attempts to create a user with hashed password and defautl 'user' role
+router.post('/signup', async ( req, res) => {
+    try {
+        const { email, password } = req.body;
+        if(!password) return res.sendStatus(400);
+        
+        // passes email and password to userDao to create a user
+        // returns the created user object if successful, null if not
+        const user = await createUser({email, password});
+        if (user) return res.sendStatus(200);
+
+    } catch (error) {
+        // checking for duplicate key error message when email is already used in db
+        if(error.name === 'MongoServerError' && error.message.includes ('E1100')) {
+            return res.sendStatus(409);
+        }
+        return res.status(500).send('Server error')
+    }
+});
+
+// login flow look up user in db and compare password to produce a token
+router.post('/login', (req, res, next) => {
+        // eslint-disable-next-line no-console
+    console.log(`Audit Log For Login User: ${req.body.email}`);
+    next();
+}, async (req, res) => {
+    const {email, password} = req.body;
+    if (!password) return res.sendStatus(400);
+
+    //look up user by email through daos
+    const user = await getUserEmail(email);
+    // if no user return 401 unathorized
+    if (!user) return res.sendStatus(401);
+
+    const hashedPassword = user.password
+    // compare provided password against the hash password
+    const isAuthenticated = await bcrypt.compare(password, hashedPassword);
+    if (isAuthenticated) {
+        const token = jwt.sign(
+            {_id: user._id.toString(), email: user.email, roles: user.roles},
+            JWT_SECRET,
+            {expiresIn: '7d'},
+        );
+        return res.status(200).json({token});
+    }
+    return res.status(401).send('unathorized');
+
+    },
+);
+
+
+export default router;
