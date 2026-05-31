32f33202980077a944622871d0ea9aad7b52972b
007GHOSTRIDER <paulr2@uw.edu>
2026-05-31 15:36:46 -0700

    add CORS config and railway deployment set up'

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const server = express();
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://movie-night-generator.web.app',
+    'https://cine-byte-api-production.up.railway.app'
+
+];
+
+const deployedUrl = process.env.RAILWAY_PUBLIC_DOMAIN
+if (deployedUrl) {
+    allowedOrigins.push(deployedUrl);
+}
+
+console.log(`allowedOrigins: ${allowedOrigins}`)
+
+server.use(cors({ origin: allowedOrigins }));
 server.use(express.json());
 
 server.use(routes);
